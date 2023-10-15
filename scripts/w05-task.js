94bfb29ca1938df8d36583b6f1cebf1b28453318
YazzAdmin <u553009672@byui.edu>
2023-10-14 21:18:29 -0700

    update

---
+++ scripts/w05-task.js
@@ -0,0 +1,32 @@
+/* W05: Programming Tasks */
+const templeElement = document.querySelector("#temples");
+let templeList = [];
+
+/* Declare and initialize global variables */
+const displayTemples = (temples) => {
+    const imageElement = document.querySelector("img");
+
+imageElement.setAttribute('src', myProfile.photo) ;
+imageElement.setAttribute('alt', `profile placeholder of ${myProfile.name} `);
+
+
+}
+
+/* async displayTemples Function */
+
+
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
